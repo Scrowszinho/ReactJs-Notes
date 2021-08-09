161b0e928c659b9c314192129ab1288db087cf12
Luis Gustavo <gustascrows@gmail.com>
2021-08-09 19:31:43 -0300

    Components: Cards start creation

---
+++ src/components/Card.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Card.css';
+import CardItem from './CardItem';
+
+function Card(props){
+    const items = props.item;
+    return(
+    <div className='CardGroup'>
+    </div>
+    );
+}
+
+export default Card;
