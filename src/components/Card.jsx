ca793b8aaf394f2ce791cd7c6b5b92286acab2d6
Luis Gustavo <gustascrows@gmail.com>
2021-08-10 18:40:37 -0300

    Components: Card components edit

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,38 @@
-import React from 'react';
+import React, { useLayoutEffect, useState } from 'react';
 import './Card.css';
-import CardItem from './CardItem';
+import FlatList from 'flatlist-react'
+import { FaTrash, FaRegFile } from 'react-icons/fa';
 
-function Card(props){
-    const items = props.item;
-    return(
-    <div className='CardGroup'>
-    </div>
+function CardItem(props) {
+    const removeSpecificNote = () => props.setNoteIndex(props.index);
+    return (
+        <div className='Note'>
+            <h1 className='NoteTitle'>{props.title}</h1>
+            <h2 className='NoteText'>{props.note}</h2>
+            <FaTrash className='TrashCanIcon' onClick={removeSpecificNote}/>
+        </div>
+    );
+}
+
+function EmptyCard() {
+    return (
+        <div className='EmptyCard'>           
+            <FaRegFile />
+        </div>
+    );
+}
+
+
+function Card(props) {
+    const items = JSON.parse(props.item);
+    return (
+        <div className='CardGroup'>
+            <FlatList list={items}
+                renderItem={(item,index) => <CardItem key={index} title={item.title} 
+                note={item.note} setNoteIndex={props.setNoteIndex} index={index} />}
+                renderWhenEmpty={() => <EmptyCard />}
+            />
+        </div>
     );
 }
 
