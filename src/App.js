63b81eb2a563c8b988ff7f39442de6868063ff1c
Luis Gustavo <gustascrows@gmail.com>
2021-08-11 22:23:57 -0300

    App remove functions

--- src/App.js
+++ src/App.js
@@ -1,44 +1,37 @@
-import React, { useState, useLayoutEffect, useEffect } from 'react';
+import React, { useLayoutEffect, useState } from 'react';
 import './App.css';
 import Card from './components/Card';
 
 function App() {
   let localStorage = window.localStorage;
   const [note, setNote] = useState('');
+  const[notesAll, setNotesAll] = useState(localStorage.getItem('notes'))
   const [title, setTitle] = useState('');
-  const [notesAll,setNotesAll] = useState(localStorage.getItem('notes'));
-  const [noteIndex,setNoteIndex] = useState();
   const saveNote = () => {
     try {
       if (note && title) {
-        let notes = new Array();
+        let notes = [];
         if(localStorage.getItem('notes')){
           notes = JSON.parse(localStorage.getItem('notes'));
         }
         notes.push({title:title,note:note});
         localStorage.setItem('notes',JSON.stringify(notes));
+        setNotesAll(localStorage.getItem('notes'));
         setNote('');
         setTitle('');
-        setNotesAll(localStorage.getItem('notes'));
       } else { }
     } catch {
 
     }
   }
 
+  const cleanAllNotes = () => {
+    localStorage.clear()
+    setNotesAll(localStorage.getItem('notes'));
+  }
   useLayoutEffect(()=>{
-    let notes = new Array();
-    if(localStorage.getItem('notes')){
-      notes = JSON.parse(localStorage.getItem('notes'));
-    }
-    notes.splice(noteIndex);
-    localStorage.setItem('notes',JSON.stringify(notes));
-  },[noteIndex]);
-
-  const cleanAllNotes = () => setNotesAll(localStorage.clear());
-  useLayoutEffect(()=>{
-  },[notesAll]);
-  return (
+  },[notesAll]);  
+    return (
     <div className="App">
       <header>
         <input value={title} onChange={t => setTitle(t.target.value)}
@@ -50,8 +43,7 @@ function App() {
         <button onClick={saveNote}>Salvar</button>
         <button onClick={cleanAllNotes}>Clear All Notes</button>
       </header>
-
-      <Card item={localStorage.getItem('notes')} setNoteIndex={setNoteIndex} />
+      <Card item={localStorage.getItem('notes')} />
     </div>
   );
 }
