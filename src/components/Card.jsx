ba7b8b3c5f6f0bb98baaa91c50f2ae2232d9542d
Luis Gustavo <gustascrows@gmail.com>
2021-08-11 22:24:07 -0300

    Remove specify note add

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,35 +1,41 @@
-import React, { useLayoutEffect, useState } from 'react';
+import React, {useLayoutEffect, useState} from 'react';
 import './Card.css';
 import FlatList from 'flatlist-react'
 import { FaTrash, FaRegFile } from 'react-icons/fa';
 
-function CardItem(props) {
-    const removeSpecificNote = () => props.setNoteIndex(props.index);
-    return (
-        <div className='Note'>
-            <h1 className='NoteTitle'>{props.title}</h1>
-            <h2 className='NoteText'>{props.note}</h2>
-            <FaTrash className='TrashCanIcon' onClick={removeSpecificNote}/>
-        </div>
-    );
-}
-
 function EmptyCard() {
     return (
-        <div className='EmptyCard'>           
+        <div className='EmptyCard'>
             <FaRegFile />
         </div>
     );
 }
 
-
 function Card(props) {
+    const[arrayList, setArrayList] = useState(window.localStorage.getItem('notes'));
     const items = JSON.parse(props.item);
+    const removeItem = (index) =>{
+        let localStorage = window.localStorage;
+        let notes = [];
+        if(localStorage.getItem('notes')){
+          notes = JSON.parse(localStorage.getItem('notes'));
+        }
+        console.log(index);
+        notes.splice(index,1);
+        localStorage.setItem('notes',JSON.stringify(notes));
+        setArrayList(window.localStorage.getItem('notes'));
+    }
+    useLayoutEffect(()=>{},[arrayList])
     return (
         <div className='CardGroup'>
             <FlatList list={items}
-                renderItem={(item,index) => <CardItem key={index} title={item.title} 
-                note={item.note} setNoteIndex={props.setNoteIndex} index={index} />}
+                renderItem={(item, index) =>
+                    <div className='Note' key={index} >
+                        <h1 className='NoteTitle'>{item.title}</h1>
+                        <h2 className='NoteText'>{item.note}</h2>
+                       <FaTrash className='TrashCanIcon'  onClick={()=>removeItem(index)} />
+                    </div>
+                }
                 renderWhenEmpty={() => <EmptyCard />}
             />
         </div>
